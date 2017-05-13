95a08f18e2d7da52bcdb8b06aeb689174205bf57
lavee singh <laveechaudharytheone@gmail.com>
2017-05-13 14:14:49 +0530

    Modified swoosh.urls to add plotting pattern, yet plotting is not supported

--- swoosh/urls.py
+++ swoosh/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.get_name, name='get_name'),
     # url(r'^$', views.index, name="index"),
-    url(r'^name/', views.get_name, name='get_name')
+    url(r'^name/', views.get_name, name='get_name'),
+    url(r'^plot/', views.matplot, name='matplot')
 ]
