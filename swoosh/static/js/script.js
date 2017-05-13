5825c70ee308fdd399e0e6f7b494bebe9d369233
lavee singh <laveechaudharytheone@gmail.com>
2017-05-13 17:19:54 +0530

    reinforced script.js in swoosh static

--- swoosh/static/js/script.js
+++ swoosh/static/js/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-  console.log("This isn't working yet");
+  $("#plot").click(function(){
+    $.ajax({
+      url: '/swoosh/plot/',
+      dataTye: 'json',
+      success: function(data){
+        $("#imagediv").html(data);
+      }
+    });
+  });
 });
