c6916194d3931f912b83a0397680b9d78a499252
lavee singh <laveechaudharytheone@gmail.com>
2017-05-13 14:21:54 +0530

    Deleted redundant static files

--- swoosh/static/js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	console.log("Only one modal is bound to appear");
-	$(".modal-link").click(function(){
-		console.log("Script is executing");
-		setTimeout(function(){$(".modal").hide();}, 2000);
-
-//		$(this).next(".bs-example-modal-lg").delay(1000).show();
-	});
-});
