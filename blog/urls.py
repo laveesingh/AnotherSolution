5d5a5231c82189af38f8ace3141f5f19e6fbf04e
lavee singh <laveechaudharytheone@gmail.com>
2017-06-13 14:47:55 +0530

    added list and detail view in urlpatterns

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url, include
-from django.views.generic import ListView, DetailView
+
 from . import views
 from blog.models import Post
 
 
 urlpatterns = [
-    url(r'^$', views.index, name="detail"),
-    url(r'^(?P<pk>\d+)$', views.detail, name="detail"),
+    url(r'^$', views.show_posts, name="show_posts"),
+    url(r'^list$', views.show_posts, name="show_posts"),
+    url(r'^details/(?P<pk>\d+)$', views.post_detail, name="post_detail"),
+    # url(r'^(?P<pk>\d+)$', views.detail, name="detail"),
 ]
