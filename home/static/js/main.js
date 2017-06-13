50fce842b10240f4bd084ccdab10e066eb67fa96
lavee singh <laveechaudharytheone@gmail.com>
2017-06-13 14:55:55 +0530

    Miscellaneous changes

---
+++ home/static/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+    // $("ul li a").on('click', function(){
+    //     console.log("pill clicked");
+    //     $(".nav-pills li").removeClass("active");
+    //     $(this).addClass("active");
+    // })
+});
