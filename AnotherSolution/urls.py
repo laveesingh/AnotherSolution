85ca51403cfa60f5e2578c86cba1acfc2f8bf0dd
lavee singh <laveechaudharytheone@gmail.com>
2017-06-13 14:44:53 +0530

    Default homepage urls set to be home.urls instead of blog.urls

--- AnotherSolution/urls.py
+++ AnotherSolution/urls.py
@@ -19,8 +19,9 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('blog.urls')),
-    url(r'^$', include('blog.urls')),
+    url(r'^$', include('home.urls')),
     url(r'^home/', include('home.urls')),
+    url(r'^stats/', include('stats.urls')),
     url(r'^swoosh/', include('swoosh.urls')),
     url(r'^testgen/', include('testgen.urls'))
 ]
