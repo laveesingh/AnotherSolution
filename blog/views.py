a3846e25f03520b25a46a2c74455ca78cb36434a
lavee singh <laveechaudharytheone@gmail.com>
2017-06-13 14:48:31 +0530

    Changed name of some of the functions for convenience

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 from blog.models import Post
 
-# Create your views here.
+
+def show_posts(request):
+    return render(request, 'blog/show_posts.html', {'object_list': Post.objects.all().order_by('-last_edit_date'), 'all': True})
 
 
-def index(request):
-    return render(request, 'blog/index.html', {'object_list': Post.objects.all().order_by('-last_edit_date')})
-
-
-def detail(request, pk):
-    return render(request, 'blog/post.html', {'post': Post.objects.filter(id=pk)[0]})
+def post_detail(request, pk):
+    return render(request, 'blog/post_detail.html', {'post': Post.objects.filter(id=pk)[0]})
