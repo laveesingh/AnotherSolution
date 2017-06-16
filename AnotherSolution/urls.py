679b70106aad368dc4717babbf6dc2e9247868e4
lavee singh <laveechaudharytheone@gmail.com>
2017-06-16 19:00:03 +0530

    Added navbar and blog made the homepage

--- AnotherSolution/urls.py
+++ AnotherSolution/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('blog.urls')),
-    url(r'^$', include('home.urls')),
+    url(r'^$', include('blog.urls')),
     url(r'^home/', include('home.urls')),
     url(r'^stats/', include('stats.urls')),
     url(r'^swoosh/', include('swoosh.urls')),
